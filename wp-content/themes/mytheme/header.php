b13388420e26f2f09baaaeb633b074121f7dfcc2
parvathyvd <parvathyvd@gmail.com>
2018-07-27 14:33:58 -0700

    changed package.json so that it will watch scss and then compile to style.css

--- wp-content/themes/mytheme/header.php
+++ wp-content/themes/mytheme/header.php
@@ -33,10 +33,6 @@
 
 <body <?php body_class(); ?>>
 <div id="page" class="site">
-    <a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'mytheme' ); ?></a>
-
-   
-
     <div class="navigation">
         <input type="checkbox" class="navigation__checkbox" id="navi-toggle">
         <label for="navi-toggle" class="navigation__label">&nbsp;
